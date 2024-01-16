1015838853ffaa09877a32db042dd4438b23ce92
UmbertoSerino <serumberto@gmail.com>
2024-01-16 18:17:34 +0100

    call Axios

--- script/script.js
+++ script/script.js
@@ -3,15 +3,30 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-
+            upiUrl: 'script/script.php',
+            albums: [],
         }
     },
     created() {
-        this.createConsole()
+        this.callAlbums()
     },
     methods: {
-        createConsole() {
-            console.log('ciao')
+        callAlbums() {
+            // Make a request for a user with a given ID
+            axios.get(this.upiUrl)
+                .then((response) => {
+                    // handle success
+                    this.albums = response.data;
+                    console.log(response.data);
+                })
+                .catch(function (error) {
+                    // handle error
+                    console.log(error);
+                })
+                .finally(function () {
+                    // always executed
+                });
+
         }
     },
 
