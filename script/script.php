c429a3ab0f634c830316f704a416743df23cfc17
UmbertoSerino <serumberto@gmail.com>
2024-01-16 17:49:22 +0100

    inserted in the script.php file, the method to send the browser the json file

--- script/script.php
+++ script/script.php
@@ -1,2 +1,6 @@
 <?php
-echo 'hey';
+// recuperiamo il file json
+$albums = file_get_contents('../json/albums.json');
+// avvertiamo il browser che sta per arrivargli un file json
+header('Content-Type: application/json');
+echo $albums;
